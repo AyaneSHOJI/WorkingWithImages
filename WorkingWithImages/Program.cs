9f77e96c8b2f7c8c00bdf1639c2eb33970618b84
AyaneSHOJI <shojiayane@gmail.com>
2022-06-15 16:00:03 +0200

    path issue ongoing

--- WorkingWithImages/Program.cs
+++ WorkingWithImages/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using static System.Console;
+
+
+string imagesFolder = Path.Combine(Environment.CurrentDirectory, "../../../images");
+
+IEnumerable<string> images = Directory.EnumerateFiles(imagesFolder);
+
+foreach(string imagePath in images)
+{
+    string thumbnailPath = Path.Combine(Environment.CurrentDirectory, "../../../images", Path.GetFileNameWithoutExtension(imagePath) + "-thumbnail" + Path.GetExtension(imagePath));
+    
+    using (Image image = Image.Load(imagePath))
+    {
+        image.Mutate(x => x.Resize(image.Width / 10, image.Height / 10));
+        image.Mutate(x => x.Grayscale());
+        image.Save(thumbnailPath);
+    }
+}
+WriteLine("Image processsing complete. View the image folder.");
